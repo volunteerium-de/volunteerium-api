41ddde9b464a5bc8e9aaf1ed6c7944c4af3086c2
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-09-12 19:03:06 +0200

    [feat] ofc/BE-014 - Refactor setups.js, Add passwordEncryption and tokenGenerator modules
    - Add extra keys and expiration times for verification and reset mechanisms
    - Add helper function for encrypting user passwords for enhanced security (passwordEncryption.js)
    - Add helper functions for generating all tokens and codes needed in the user authentication process (tokenGenerator.js)

--- setups.js
+++ setups.js
@@ -1,5 +1,7 @@
 "use strict";
+
 require("dotenv").config();
+
 module.exports = {
   PORT: process.env?.PORT || 8000,
   HOST: process.env?.HOST || "127.0.0.1",
@@ -7,11 +9,15 @@ module.exports = {
   CLIENT_URL: process.env?.CLIENT_URL,
   MONGODB_URI: process.env?.MONGODB_URI,
   SECRET_KEY: process.env?.SECRET_KEY,
-  PAGE_SIZE: process.env?.PAGE_SIZE || 25,
+  PAGE_SIZE: process.env?.PAGE_SIZE || 5,
   ACCESS_KEY: process.env?.ACCESS_KEY,
-  ACCESS_EXP: process.env?.ACCESS_EXP || "30m",
+  ACCESS_EXP: process.env?.ACCESS_EXP || "1d",
   REFRESH_KEY: process.env?.REFRESH_KEY,
   REFRESH_EXP: process.env?.REFRESH_EXP || "2d",
+  VERIFY_EMAIL_KEY: process.env?.VERIFY_EMAIL_KEY,
+  VERIFY_EMAIL_EXP: process.env?.VERIFY_EMAIL_EXP || "7d",
+  RESET_PASSWORD_KEY: process.env?.RESET_PASSWORD_KEY,
+  RESET_PASSWORD_EXP: process.env?.RESET_PASSWORD_EXP || "2m",
   NODE_ENV: process.env.NODE_ENV == "production" ? true : false, //! in development false - // Set secure to true if using HTTPS / after deployment
   AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME,
   AWS_S3_BUCKET_REGION: process.env.AWS_S3_BUCKET_REGION,
