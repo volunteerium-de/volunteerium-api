6352cfe24b707d2271ae391568d37f9783bd82eb
Burak Karaman <150926922+karamanburak@users.noreply.github.com>
2024-09-16 14:57:09 +0200

    [Refactor] bk/BE-IdValidation - Update idValidation.js

--- src/middlewares/idValidation.js
+++ src/middlewares/idValidation.js
@@ -1,9 +1,10 @@
 "use strict";
 
 const { mongoose } = require("../configs/dbConnection");
+const { CustomError } = require("../errors/customError");
 
 module.exports = (req, res, next) => {
   const idIsValid = mongoose.Types.ObjectId.isValid(req.params.id); //* builtin method
-  if (!idIsValid) throw new Error("Id is not valid!");
+  if (!idIsValid)  throw new CustomError("Id is not valid!");
   next();
 };
