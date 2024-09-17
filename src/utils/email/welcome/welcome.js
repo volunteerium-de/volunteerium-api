d4b7306eb190be5d5617f3c1db37cb0110e95ec2
Burak Karaman <karaman.buraak@gmail.com>
2024-09-17 11:00:00 +0200

    [Refactor] - bk/BE-011 - Update reset code & environment

--- src/utils/email/welcome/welcome.js
+++ src/utils/email/welcome/welcome.js
@@ -3,7 +3,7 @@ const path = require("path");
 const { CLIENT_URL } = require("../../../../setups");
 
 const getWelcomeEmailHtml = (firstName, token) => {
-  let html = fs.readFileSync(path.join(__dirname, "welcomeEmail.html"), "utf8");
+  let html = fs.readFileSync(path.join(__dirname, "welcome.html"), "utf8");
   html = html.replace("{{firstName}}", firstName);
   html = html.replace(/{{token}}/g, token);
   html = html.replace(/{{clientUrl}}/g, CLIENT_URL);
