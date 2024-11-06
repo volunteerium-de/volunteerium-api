e13e58414b7f57c69c85d31457fe345c0c96ea41
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-11-06 13:42:36 +0100

    Update isActiveEvent middleware to allow admins to access inactive events

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -139,8 +139,10 @@ module.exports = {
   isActiveEvent: async (req, res, next) => {
     // if (!NODE_ENV) return next();
     const event = await Event.findById(req.params.id);
-    if (req.user.userType === "admin" || event.isActive) {
+    if (event.isActive) {
       return next(); // Event is active
+    } else if (req.user && req.user.userType === "admin") {
+      return next(); // Admins can manage inactive events
     } else {
       throw new CustomError(req.t(translations.permission.isActiveEvent), 403);
     }
